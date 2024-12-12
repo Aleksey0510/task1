709c2ff2cc258aba8c1e2030438ccc6d19a5ac5f
Aleksey <kolesov000@mail.ru>
2024-12-12 21:10:15 +0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int ticket = 5000; // стоимость билета  //
+        int mile = 20; // количество рублей для одной мили //
+
+        int bonus = ticket / mile;
+        System.out.println(bonus + " бонусных миль ");
+
+
     }
 }
